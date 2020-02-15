b9efc5e248e76f4def16c08794113ccc4843286b
egrassa <bacek90@mail.ru>
2020-02-15 22:37:31 +0300

    auto commit

--- gendiff/format/text.py
+++ gendiff/format/text.py
@@ -8,28 +8,28 @@ NOT = 'not'
 CHG = 'change'
 
 
-def gen_string(indent, diff, key, status):
+def gen_string(indent, value, key, status):
     if status == ADD:
         prefix = '+'
     elif status == DEL:
         prefix = '-'
     elif status == CHG:
-        return gen_string(indent, diff, key, DEL) + gen_string(indent, diff, key, ADD)
+        return  gen_string(indent, value[1], key, ADD) + gen_string(indent, value[0], key, DEL)
     else:
         prefix = ' '
-    if diff == None:
+    if value == None:
         return "{}{} {}: ".format(indent[:-2], prefix, key)
-    return "{}{} {}: {}\n".format(indent[:-2], prefix, key, get_value(diff, key))
+    return "{}{} {}: {}\n".format(indent[:-2], prefix, key, value)
 
 
 def render_text(diff, deep=1):
     indent = '    ' * deep
     render = '{\n'
-    for key in diff:
+    for key in sorted(diff):
         if isinstance(get_value(diff, key), dict):
             render += gen_string(indent, None, key, get_status(diff, key))
             render += render_text(get_value(diff, key), deep + 1)
         else:
-            render += gen_string(indent, diff, key, get_status(diff, key))
+            render += gen_string(indent, get_value(diff, key), key, get_status(diff, key))
     render += indent[:-4] + '}\n'
     return render
