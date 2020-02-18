9bcaa1d71e376dc2c4247d09f5439f6446803b78
egrassa <bacek90@mail.ru>
2020-02-18 21:55:45 +0300

    auto commit

--- gendiff/format/plain.py
+++ gendiff/format/plain.py
@@ -8,7 +8,9 @@ NOT = 'not'
 CHG = 'change'
 
 
-def gen_string(value, key, status):
+def gen_string(value, key, status, path_keys=[]):
+    if path_keys:
+        key = '.'.join(path_keys) + '.' + key
     if isinstance(value, dict):
         value = 'complex value'
     if status == ADD:
@@ -20,47 +22,14 @@ def gen_string(value, key, status):
     return ''
 
 
-def render_plain(diff):
-    print(diff)
+def render_plain(diff, path_keys=[]):
     render = ''
-    for key in diff:
+    for key in sorted(diff):
         if get_status(diff, key) == NOT and isinstance(get_value(diff, key), dict):
-            render +='{}.{}'.format(key, render_plain(get_value(diff, key)))
+            path_keys.append(key)
+            render += render_plain(get_value(diff, key), path_keys)
+            path_keys.pop()
             continue
-        # print(key)
-        render += gen_string(get_value(diff,key), key, get_status(diff, key))
+        render += gen_string(get_value(diff, key), key,
+                             get_status(diff, key), path_keys)
     return render
-
-
-
-# def gen_string_del(key):
-#     return "Property '{}' was removed\n".format(key)
-
-
-# def gen_string_add(key, value):
-#     if isinstance(value, dict):
-#         value = 'complex value'
-#     return "Property '{}' was added with value: '{}'\n".format(key, value)
-
-
-# def gen_string_change(key, value):
-#     return "Property '{}' was changed. From '{}' to '{}'\n"\
-#             .format(key, value[1]['value'], value[0]['value'])
-
-
-# def render_plain(diff, path_key=''):
-#     render = '1'
-#     if path_key:
-#         path_key = path_key + '.'
-#     for key in diff:
-#         if isinstance(diff[key], dict):
-#             if diff[key]['change'] == 'del':
-#                 render += gen_string_del(path_key+key)
-#             if diff[key]['change'] == 'add':
-#                 render += gen_string_add(path_key+key, diff[key]['value'])
-#             if (diff[key]['change'] == 'not' and
-#                     isinstance(diff[key]['value'], dict)):
-#                 render += render_plain(diff[key]['value'], path_key+key)
-#         elif isinstance(diff[key], list):
-#             render += gen_string_change(path_key+key, diff[key])
-#     return render
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,24 +4,6 @@ from gendiff import parsers
 from gendiff import format
 
 
-# def generate_diff(file1, file2):
-#     diff = {}
-#     for key in file1:
-#         if key in file2:
-#             if isinstance(file1[key], dict) and isinstance(file2[key], dict):
-#                 diff[key] = {'value': generate_diff(file1[key], file2[key]),
-#                              'change': 'not'}
-#             elif file1[key] != file2[key]:
-#                 diff[key] = [{'value': file2[key], 'change': 'add'},
-#                              {'value': file1[key], 'change': 'del'}]
-#             elif file1[key] == file2[key]:
-#                 diff[key] = {'value': file1[key], 'change': 'not'}
-#         else:
-#             diff[key] = {'value': file1[key], 'change': 'del'}
-#     for key in file2:
-#         if key not in file1:
-#             diff[key] = {'value': file2[key], 'change': 'add'}
-#     return diff
 ADD = 'add'
 DEL = 'del'
 NOT = 'not'
@@ -42,7 +24,7 @@ def generate_diff(file1, file2):
     equal_keys = set(file1.keys()).intersection(set(file2.keys()))
 
     for key in equal_keys:
-        if isinstance(file1[key],dict) and isinstance(file2[key], dict):
+        if isinstance(file1[key], dict) and isinstance(file2[key], dict):
             diff.update({key: (generate_diff(file1[key], file2[key]), NOT)})
         elif file1[key] == file2[key]:
             diff.update(gen_key_process(file1, (key,), NOT))
