2ea93768c9518406ebbc9494ab61c7c04598e797
egrassa <bacek90@mail.ru>
2020-02-20 22:23:04 +0300

    auto commit

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -14,24 +14,24 @@ def gen_key_process(file, keys, change):
     return record_to_diff
 
 
-def generate_diff(file1, file2):
+def generate_diff(prs_file1, prs_file2):
     diff = {}
-    keys_first_pars_obj = set(file1.keys())
-    keys_second_pars_obj = set(file2.keys())
+    keys_first_pars_obj = set(prs_file1.keys())
+    keys_second_pars_obj = set(prs_file2.keys())
     del_keys = keys_first_pars_obj.difference(keys_second_pars_obj)
     add_keys = keys_second_pars_obj.difference(keys_first_pars_obj)
-    equal_keys = set(file1.keys()).intersection(set(file2.keys()))
+    equal_keys = set(prs_file1.keys()).intersection(set(prs_file2.keys()))
 
     for key in equal_keys:
-        if isinstance(file1[key], dict) and isinstance(file2[key], dict):
-            diff.update({key: (generate_diff(file1[key], file2[key]), NOT)})
-        elif file1[key] == file2[key]:
-            diff.update(gen_key_process(file1, (key,), NOT))
+        if isinstance(prs_file1[key], dict) and isinstance(prs_file2[key], dict):
+            diff.update({key: (generate_diff(prs_file1[key], prs_file2[key]), NOT)})
+        elif prs_file1[key] == prs_file2[key]:
+            diff.update(gen_key_process(prs_file1, (key,), NOT))
         else:
-            diff.update({key: ((file1[key], file2[key]), CHG)})
+            diff.update({key: ((prs_file1[key], prs_file2[key]), CHG)})
 
-    diff.update(gen_key_process(file1, del_keys, DEL))
-    diff.update(gen_key_process(file2, add_keys, ADD))
+    diff.update(gen_key_process(prs_file1, del_keys, DEL))
+    diff.update(gen_key_process(prs_file2, add_keys, ADD))
     return diff
 
 
@@ -43,9 +43,9 @@ def main():
                         default='text', help="set format of output",
                         type=format.get_render)
     args = parser.parse_args()
-    file1 = parsers.parse_file(args.first_file)
-    file2 = parsers.parse_file(args.second_file)
-    diff = generate_diff(file1, file2)
+    prs_file1 = parsers.parse_file(args.first_file)
+    prs_file2 = parsers.parse_file(args.second_file)
+    diff = generate_diff(prs_file1, prs_file2)
     print(args.format(diff))
 
 
